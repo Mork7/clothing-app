05bae60237a9ddcef0511d29aec167dfb3c83fe4
Mork <mfinerty1@hotmail.com>
2024-02-01 10:16:46 -0500

    change company name in header

--- src/components/MuiAppBar.jsx
+++ src/components/MuiAppBar.jsx
@@ -29,12 +29,12 @@ export default function MenuAppBar({ openModal, openSidebar }) {
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 , fontFamily: "Gamja Flower"}}>
-              Your Company Name
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 , fontFamily: "Gamja Flower", fontSize: "xx-large"}}>
+              Lindsay&apos;s Secret
             </Typography>
             <div>
               <button
-                className="flex rounded-lg items-center bg- border-2 py-2 px-4 border-black hover:bg-purple-300"
+                className="flex rounded-lg items-center bg- border-2 spy-2 px-4 border-black hover:bg-purple-300"
                 onClick={openModal}
               >
                 <img className="w-5 mr-1" src={cartIcon} alt="" />
